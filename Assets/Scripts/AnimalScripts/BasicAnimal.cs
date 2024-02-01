97a88fe0b8868777422cb08c9a24678bbf367c1b
LewisAt <144160860+LewisAt@users.noreply.github.com>
2024-01-31 19:42:46 -0500

    safety push

--- Assets/Scripts/AnimalScripts/BasicAnimal.cs
+++ Assets/Scripts/AnimalScripts/BasicAnimal.cs
@@ -6,9 +6,15 @@ using UnityEngine.AI;
 public class BasicAnimal : MonoBehaviour
 {
     NavMeshAgent agent;
+    private GameObject ModelToMove;
+    private Vector3 DefaultModelPositon;
     public int navMeshMaskNumber;
+    public float MovementStrength;
+    public float MoveSpeed = 1f;
+    public bool waddle = false;
+    private float moveCap;
 
-
+    private int MovementDirection = 0;
 
     private void Awake()
     {
@@ -16,17 +22,31 @@ public class BasicAnimal : MonoBehaviour
     }
     private void Start()
     {
+        ModelToMove = this.transform.GetChild(0).gameObject;
+        DefaultModelPositon = ModelToMove.transform.position;
         Roam();
 
         StartCoroutine(startRoam());
 
     }
-
+    void waddleAnimation()
+    {
+        if(agent.velocity.sqrMagnitude > 0.1f)
+        {
+            // up motion
+            float MotionAdition = (Time.deltaTime * MovementStrength) * MovementDirection;
+            ModelToMove.transform.position = new Vector3(DefaultModelPositon.x,DefaultModelPositon.y + MotionAdition,DefaultModelPositon.z);     
+        }
+        else
+        {
+            moveCap = 0;
+        }
+    }
     IEnumerator startRoam()
     {
         while (true)
         {
-            yield return new WaitForSeconds(15);
+            yield return new WaitForSeconds(Random.Range(2,10));
             Roam();
         }
     }
