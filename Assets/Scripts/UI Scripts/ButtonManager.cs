d6fb2cf6786a0eebceb586a5bd2584362791362c
TheCoolBeansMan <abetan2003@gmail.com>
2024-01-31 22:58:23 -0500

    addedtitlescreen

--- Assets/Scripts/UI Scripts/ButtonManager.cs
+++ Assets/Scripts/UI Scripts/ButtonManager.cs
@@ -7,33 +7,9 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour
 {
     public int sceneNum;
-    public GameObject currMenu;
-    public GameObject nextMenu;
-
-    public GameObject currInfo;
-    public GameObject OtherInfoA;
-    public GameObject OtherInfoB;
-    public GameObject OtherInfoC;
 
     public void NextScene()
     {
         SceneManager.LoadScene(sceneNum);
-    }
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    public void ToNextMenu()
-    {
-        currMenu.SetActive(false);
-        nextMenu.SetActive(true);
-    }
-    public void SummonAnimalInfo()
-    {
-        OtherInfoA.SetActive(false);
-        OtherInfoB.SetActive(false);
-        OtherInfoC.SetActive(false);
-        currInfo.SetActive(true);
     }
 }
