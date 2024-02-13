2a9ba066e55103c5169f2e9ee668f21ddaf12459
TheCoolBeansMan <abetan2003@gmail.com>
2024-02-12 19:30:27 -0500

    Revert "New scenes and edits"
    This reverts commit 1ac37c7630b901a1005abf864d49e348058697e7.

--- Assets/Scripts/Info intraction.cs
+++ Assets/Scripts/Info intraction.cs
@@ -5,30 +5,14 @@ using UnityEngine.UI;
 
 public class Infointraction : MonoBehaviour
 {
-    [TextArea(15, 20)]
+
     public string infoTodisplay;
     public Text TextBoxToReveal;
     public GameObject CanvasBase;
-
     // Start is called before the first frame update
     public void enableAll()
     {
-        if(CanvasBase.activeSelf)
-        {
-            CanvasBase.SetActive(false);
-            return;
-        }
         CanvasBase.SetActive(true);
         TextBoxToReveal.text = infoTodisplay;
-        StartCoroutine(Disable());
-    }
-    IEnumerator Disable()
-    {
-        yield return new WaitForSeconds(15);
-        if(CanvasBase.activeSelf)
-        {
-            CanvasBase.SetActive(false);
-
-        }
     }
 }
