877930fa29303b7bcb044d968f97bf93400b8f94
TheCoolBeansMan <abetan2003@gmail.com>
2024-02-12 19:23:56 -0500

    Revert "Revert "Merge branch 'Fixing-BehaviorBugs' of https://github.com/LewisAt/Everglades-MDC into Fixing-BehaviorBugs""
    This reverts commit 8f18441a23da17b35435da432d764e265605860b.

--- Assets/Scripts/UI Scripts/ButtonManager.cs
+++ Assets/Scripts/UI Scripts/ButtonManager.cs
@@ -7,9 +7,23 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour
 {
     public int sceneNum;
+    public GameObject animalMenu;
+    public GameObject interactableMenu;
 
     public void NextScene()
     {
         SceneManager.LoadScene(sceneNum);
+    }
+
+    public void animalTransition()
+    {
+        animalMenu.SetActive(false);
+        interactableMenu.SetActive(true);
+    }
+
+    public void interactableTranstition()
+    {
+        animalMenu.SetActive(true);
+        interactableMenu.SetActive(false);
     }
 }
